0bdf3ee0f189e4ffa7a3a7c6df4505ce34b1dcf0
MadisonUrbanist <ryanvergeront@gmail.com>
2013-03-30 21:07:11 -0500

    add authentication to micropost controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,18 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
-  	def new
-  		if signed_in? 
-  			redirect_to root_path
-  		else
+
+	def new
+		if signed_in? 
+			redirect_to root_path
+		else
 			@user = User.new
 		end
-  	end
+	end
 
 	def show
 		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page]) 
 	end
 
 	def create
@@ -61,13 +63,6 @@ class UsersController < ApplicationController
 	end
 
   private
-
-    def signed_in_user
-   	  unless signed_in?
-   	  	store_location
-      	redirect_to signin_url, notice: "Please sign in."
-  	  end	
-    end
 
     def correct_user
       @user = User.find(params[:id])
